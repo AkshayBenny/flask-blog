2520ff031efaf4ad20e9cf346aa986ad8ba36cbe
Akshay Benny <akshay@mx-zenbook>
2024-01-12 16:24:48 +0000

    login screen

--- server/website/__init__.py
+++ server/website/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, \
+    unset_jwt_cookies, jwt_required, JWTManager
+from datetime import datetime, timedelta, timezone
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -11,8 +14,11 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'helloworld'
+    app.config["JWT_SECRET_KEY"] = "SDGW4WER9SDFSQWQE0F9D0F9JSDFNLQ"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
+    jwt = JWTManager(app)
 
     from .views import views
     from .auth import auth
