147e4ab0ca9702d58b44ee1c9548abb7eff4d43e
Akshay Benny <akshay@mx-zenbook>
2024-01-16 19:15:04 +0000

    edit

--- server/app.py
+++ server/app.py
@@ -2,11 +2,13 @@ from website import create_app
 from flask_cors import CORS
 from website.limiter_setup import limiter
 from flask_wtf.csrf import CSRFProtect
+from flask_talisman import Talisman
 
 if __name__ == "__main__":
     app = create_app()
     app.config['SECRET_KEY'] = 'DFAIGARG9AERGHAGFAFGKAFGADFGLHBA'
-    csrf = CSRFProtect(app)
-    limiter.init_app(app)
+    CSRFProtect(app)
+    Talisman(app)
     CORS(app)
+    limiter.init_app(app)
     app.run(debug=True)
